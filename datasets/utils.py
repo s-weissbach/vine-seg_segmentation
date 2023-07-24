3c8dda3efe2276d4e9f27dc0ff2096c9a9c0ee46
j-milkovits <j.milkovits.t@posteo.net>
2023-07-24 11:29:52 +0200

    add train/val/test split on file-level to generation

--- datasets/utils.py
+++ datasets/utils.py
@@ -1,5 +1,8 @@
 import json
 import os
+from shutil import copyfile
+
+import numpy as np
 
 
 def transform_segmentations(segmentations, width, height):
@@ -14,14 +17,40 @@ def transform_segmentations(segmentations, width, height):
     return res
 
 
-# TODO: add a way to split files into folders (train-val-test split)
-def coco_seg_to_yolov8(coco_dict_path, output_path):
-    with open(coco_dict_path, "r") as file:
+def split_indices(length, splits):
+    splits_adjusted = [int(length * split) for split in splits]
+
+    # Generate a list of random integers between 0 and the length of the list (exclusive)
+    indices = np.arange(length)
+
+    # Shuffle the indices to distribute them randomly
+    np.random.shuffle(indices)
+
+    # Calculate the starting and ending indices for each split
+    split_points = np.cumsum(splits_adjusted)[:-1]
+    split_lists = np.split(indices, split_points)
+
+    return split_lists
+
+
+def coco_seg_to_yolov8(coco_path, output_path, splits):
+    assert (
+        len(splits) == 3
+    ), "Please provide three values for splits. If you don't need a validation or test set, just provide 0 as a value. Example: [1, 0, 0]"
+
+    coco_dict_name = [file for file in os.listdir(coco_path) if ".json" in file][0]
+    full_file_path = os.path.join(coco_path, coco_dict_name)
+
+    with open(full_file_path, "r") as file:
         coco_dict = json.load(file)
 
+    num_images = len(coco_dict["images"])
+
+    train, val, test = split_indices(num_images, splits)
+
     # loop over images
-    for image in coco_dict["images"]:
-        id, width, height = image["id"], image["width"], image["height"]
+    for idx, image in enumerate(coco_dict["images"]):
+        id, width, height, file_name = image["id"], image["width"], image["height"], image["file_name"]
         # get annotations
         annotations = filter(lambda x: x["image_id"] == id, coco_dict["annotations"])
         # check why this is an array  of arrays
@@ -31,9 +60,18 @@ def coco_seg_to_yolov8(coco_dict_path, output_path):
         # transform from pixel to percent
         segmentations_percent = transform_segmentations(segmentations, width, height)
 
+        # get path for file depending on split
+        slug = "train" if idx in train else "val" if idx in val else "test"
+        final_path = os.path.join(output_path, slug)
+
         # write to disk
-        file_path = os.path.join(output_path, f"{id}.txt")
-        with open(file_path, "w") as file:
+        file_path_label = os.path.join(final_path, "labels", f"{id}.txt")
+        with open(file_path_label, "w") as file:
             for segmentation in segmentations_percent:
                 file.write(" ".join([str(val) for val in segmentation]))
                 file.write("\n")
+
+        # copy image over to yolo folder
+        file_path_image_src = os.path.join(coco_path, file_name)
+        file_path_image_dest = os.path.join(final_path, "images", file_name)
+        copyfile(file_path_image_src, file_path_image_dest)
